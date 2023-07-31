b7b483f8f69ccc002891f1bd3128710a651ee707
Si Nguyen <130967183+copcopne@users.noreply.github.com>
2023-07-31 20:08:42 +0700

    lam lai nut go to top

--- js/script.js
+++ js/script.js
@@ -1,14 +1,14 @@
-/* go to top*/
-function scrollFunction()
-{
-    let goTop = document.getElementById("goTop");
-    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20)
-        goTop.style.display = "block";
-    else
-        goTop.style.display = "none";
-}
-window.onscroll = scrollFunction();
-function goToTop()
-{
-    window.scrollTo({top: 0, behavior: 'smooth'});
-}
+$(document).ready(function() {
+    $(window).scroll(function() {
+        var show = 100;
+        if($(this).scrollTop() > show) {
+            $('#goTop').fadeIn();
+        } else {
+            $('#goTop').fadeOut();
+        }
+    });
+    $('#goTop').click(function() {
+        $('html, body').animate({scrollTop : 0}, 0);
+        return false;
+    });
+});
