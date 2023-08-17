53405faa2a6ba9aa04a864f8dbbbee91c1cfafea
Si Nguyen <130967183+copcopne@users.noreply.github.com>
2023-08-17 21:27:53 +0700

    giam bot code trong html

--- js/load.js
+++ js/load.js
@@ -62,7 +62,56 @@ function loadnav()
             }
         })
 }
-function loadscript() {
+function loadhtmlstruct() {
+    $("body").prepend(`
+        <header class="head flex">
+        <div class ="l-head flex logo">
+            <div>
+                <a href="index.html">
+                    <img class="icon" src="src/logo.png" alt="logo" />
+                    <h1  style="font-size:20px;">
+                        <span class="sub1">Tận tình</span> - <span class="sub2">Chất lượng</span>
+                    </h1>
+                </a>
+            </div>
+            <div class="text">
+                <h1 style="font-size:28px;">BỆNH VIỆN MỞ</h1>
+                <h2 style="font-size:24px;">THÀNH PHỐ HỒ CHÍ MINH</h2>
+            </div>
+        </div>
+        <div class="r-head flex">
+            <div>
+                <a href="https://goo.gl/maps/m6bpzZ9u9XM9eWrd8" target="_blank">
+                    <address class="highlight">
+                        371 Nguyễn Kiệm, Phường 3, Gò Vấp, Thành phố Hồ Chí Minh
+                    </address>
+                </a>
+            </div>
+            <div>Giờ làm việc: <span class="highlight">06:00 - 16:30</span></div>
+            <div>Liên hệ: <a href="tel:02839300210"><span class="highlight">028 3930 0210</span></a></div>
+        </div>
+        </header>  
+        <nav>
+            <div class="nav sticky flex">
+                <ul class="menu">
+                </ul>
+            </div>
+        </nav>
+    `);
+    $("main").append(`<button id="goTop" title="Go to top"><span>TOP</span></button>`);
+    $("main").after(`
+        <footer class = "flex">
+            <p>
+                Copyright © Bệnh viện Mở TPHCM
+                <br>
+                Thiết kế: Nguyễn Trương Quý Sĩ, Đường Dương Thái Tuấn
+            </p>
+            <hr>
+            <p>
+                Cơ sở chính: 371 Nguyễn Kiệm, Phường 3, Gò Vấp, Thành phố Hồ Chí Minh
+            </p>
+        </footer>
+`);
     $("#goTop").hide();
     let stickyTop =$(".sticky").offset().top;
     $("#goTop").click(function() {
@@ -85,5 +134,5 @@ function loadscript() {
 window.onload = function()
 {
     loadnav();
-    loadscript();
+    loadhtmlstruct();
 }
