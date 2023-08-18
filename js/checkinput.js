4e4b37de5b095f75ce40b2363c3dbc1bf297baa2
hyskel <ttuan.simple@gmail.com>
2023-08-18 13:35:44 +0700

    thêm comment

--- js/checkinput.js
+++ js/checkinput.js
@@ -1,22 +1,21 @@
 $(document).ready(function(){
     $(".submit").on("click",function(){
-        let inputs = $("input:not(.submit), select, textarea");
-        let n = 0;
-        let valid = true;
-        for(let i of inputs)
+        let inputs = $("input:not(.submit), select, textarea"); //các nút
+        let valid = true; //kiểm tra rỗng
+        for(let i of inputs) //kiểm tra từng nút
         {
-            if(i.hasAttribute('required') && i.value.length == 0)
+            if(i.hasAttribute('required') && i.value.length == 0) //nếu required và rỗng
             {
-                i.classList.add("alert");
+                i.classList.add("alert"); //thêm class để bật hiệu ứng
                 i.focus();
-                i.addEventListener('animationend',() => {
+                i.addEventListener('animationend',() => { //xóa class khi hiệu ứng end
                     i.classList.remove("alert");
                 })
                 valid = false;
                 break;
             }
         }
-        if (valid)
+        if(valid) 
         $("form").submit();
     })
    })
