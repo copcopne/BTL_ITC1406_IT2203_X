064f8baa7537b197f03f9e14f2ded1bb4d931da4
hyskel <ttuan.simple@gmail.com>
2023-08-18 14:02:28 +0700

    đọc q-a.json

--- js/q-a.js
+++ js/q-a.js
@@ -1,7 +1,22 @@
 $(document).ready(function(){
-    $(".q").on("click",function(){
+     fetch("json/q-a.json").then(res=>res.json()).then(data => {
+        for (let d of data)
+        {
+            let h = `
+            <li>
+            <div class = "q">
+                <span class = "arrow"></span>
+                <span>${d.question}</span>
+            </div>
+            <div class = "a">${d.answer}
+            </div>
+            </li>`
+            if(d === null) continue;
+            $(".faq").append(h);
+        }
+     })
+     $(document).on("click",".q",function(){
         $(this).parent().find(".a").toggleClass("a-open");
         $(this).find(".arrow").toggleClass("arrow-rotate");
     });
-    
 });
