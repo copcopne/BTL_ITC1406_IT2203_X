860764296618e2cdf54657b28da5f1f208b0037a
Si Nguyen <130967183+copcopne@users.noreply.github.com>
2023-08-21 21:05:39 +0700

    he

--- js/mobile.js
+++ js/mobile.js
@@ -1,11 +1,22 @@
 function openMenu(){
     $(".nav").toggleClass("hidden");
     $(".nav").toggleClass("appear");
-    $(".overlay").toggle();
-    $(".overlay").toggleClass("fadein");
+    if($(".nav").hasClass("appear") === true) {
+        setTimeout(()=>{
+            $(".overlay").toggle();
+            $(".overlay").toggleClass("fadein");
+        }, 500);
+    }
+    else {
+        $(".overlay").toggle();
+        $(".overlay").toggleClass("fadein");
+    }
 }
 $(document).on("click",".menu a",function(event){
     if($(window).width()<=800)
             $(event.target).parent().children("ul").toggle();
+})
+$(document).on("click",".overlay",function () {
+    openMenu();
 })
 
