8e55d8d23ca3823a1c5cd04e9dbf8983e01cf103
hyskel <ttuan.simple@gmail.com>
2023-08-21 19:36:20 +0700

    fix responsive

--- js/mobile.js
+++ js/mobile.js
@@ -5,5 +5,13 @@ function openMenu(){
     $(".overlay").toggleClass("fadein");
 }
 $(document).on("click",".menu a",function(event){
-    $(event.target).parent().find("ul").toggle();
+    if($(window).width()<=800)
+            $(event.target).parent().children("ul").toggle();
+})
+
+$(document).on("resize ready",function(){
+    if($(window).width() <= 800)
+    {
+        $(".menu ul").attr("style", "display:none");
+    }
 })
